fca25cc4ba16b9a93d9672dd3d9a613bfaa7f585
Karthikadyapady <karthik2fast4u@gmail.com>
2016-10-14 14:29:26 -0400

    Update Add.php

--- Latest_Working/Add.php
+++ Latest_Working/Add.php
@@ -1,19 +1,8 @@
 <html>
    <body>
       <?php
-         $servername = "localhost";
-         $username = "root";
-         $password = "";
-         $dbname = "movies";
-         
-         // Create connection
-         $conn = mysqli_connect($servername, $username, $password, $dbname);
-         
-         // Check connection
-         if (!$conn) {
-           die("Connection failed: " . mysqli_connect_error());
-         }
-         
+    include 'include.php';
+      
          $genre = $_POST['genre'];
          $Country = $_POST['Country'];
          $Price = $_POST['Price'];
